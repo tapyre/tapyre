437977492632749181311e252e8c1ba7f26b4e3d
Raphael Ladinig <mail@raphaelladinig.com>
2025-10-13 16:58:43 +0200

    improve frontend

--- src/frontend/gtk_frontend.py
+++ src/frontend/gtk_frontend.py
@@ -10,6 +10,7 @@ from runtime.plugin_loader import PluginLoader
 from runtime.config_loader import ConfigLoader
 from implementations.ollama_llm import OllamaLLM
 import yaml
+import threading
 
 
 class MyWindow(Gtk.Window):
@@ -33,22 +34,28 @@ class MyWindow(Gtk.Window):
         GtkLayerShell.set_layer(self, GtkLayerShell.Layer.OVERLAY)
         GtkLayerShell.set_keyboard_mode(self, GtkLayerShell.KeyboardMode.EXCLUSIVE)
 
-        self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.box.set_name("main-box")
         self.add(self.box)
 
+        input_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        self.box.pack_start(input_box, False, False, 10)
+
         prompt_label = Gtk.Label(label=" > ")
-        self.box.pack_start(prompt_label, False, False, 0)
+        input_box.pack_start(prompt_label, False, False, 0)
 
         self.entry = Gtk.Entry()
+        self.entry.set_name("user-input")
         self.entry.connect("activate", self.on_entry_activate)
-        self.box.pack_start(self.entry, True, True, 0)
-
+        input_box.pack_start(self.entry, True, True, 0)
 
     def on_entry_activate(self, entry):
         text = entry.get_text()
-        print("You entered:", text)
+        entry.set_text("")
+        self.hide()
+        thread = threading.Thread(target=self.agent.ask, args=(text,))   
+        thread.start()
         Gtk.main_quit()
-        self.agent.ask(text)
 
     def on_key_press(self, widget, event):
         if event.keyval == Gdk.KEY_Escape:
